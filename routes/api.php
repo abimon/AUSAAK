a2a4cad5a81aa40e7c243a27bdde47b166bc7a8a
abimon <eddyabby59@gmail.com>
2024-06-13 22:23:29 +0300

    new

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 Route::controller(UserController::class)->group(function(){
     Route::post('/user/store', 'store');
 });
