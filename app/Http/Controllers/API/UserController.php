e7ead19cb0bca6b219698ba45d4f1db149745f4e
abimon <eddyabby59@gmail.com>
2024-06-13 22:14:15 +0300

    new

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -35,53 +35,74 @@ class UserController extends Controller
 
     public function store()
     {
-        try {
-            // $validateUser = Validator::make(
-            //     request()->all(),
-            //     [
-            //         'email' => 'required|email|unique:users,email',
-            //         'password' => 'required',
-            //         'fname' => 'required|string',
-            //         'lname' => 'required|string',
-            //         'contact' => 'require|min:9',
-            //     ]
-            // );
+        // try {
+        //     // $validateUser = Validator::make(
+        //     //     request()->all(),
+        //     //     [
+        //     //         'email' => 'required|email|unique:users,email',
+        //     //         'password' => 'required',
+        //     //         'fname' => 'required|string',
+        //     //         'lname' => 'required|string',
+        //     //         'contact' => 'require|min:9',
+        //     //     ]
+        //     // );
 
-            // if ($validateUser->fails()) {
-            //     return response()->json([
-            //         'status' => false,
-            //         'message' => 'validation error',
-            //         'errors' => $validateUser->errors()
-            //     ], 401);
-            // }
+        //     // if ($validateUser->fails()) {
+        //     //     return response()->json([
+        //     //         'status' => false,
+        //     //         'message' => 'validation error',
+        //     //         'errors' => $validateUser->errors()
+        //     //     ], 401);
+        //     // }
 
-            $user = User::create([
-                'fname' => request()->fname,
-                'lname' => request()->lname,
-                'email' => request()->email,
-                'contact' => request()->contact,
-                'current_residence' => request()->current_residence,
-                'profile' => request()->profile,
-                'gender' => request()->gender,
-                'chapter' => request()->chapter,
-                'grad_year' => request()->grad_year,
-                'password' => request()->password,
-                'role' => request()->role,
-                'inst' => request()->inst,
-            ]);
+        //     $user = User::create([
+        //         'fname' => request()->fname,
+        //         'lname' => request()->lname,
+        //         'email' => request()->email,
+        //         'contact' => request()->contact,
+        //         'current_residence' => request()->current_residence,
+        //         'profile' => request()->profile,
+        //         'gender' => request()->gender,
+        //         'chapter' => request()->chapter,
+        //         'grad_year' => request()->grad_year,
+        //         'password' => request()->password,
+        //         'role' => request()->role,
+        //         'inst' => request()->inst,
+        //     ]);
 
-            return response()->json([
-                'user' => $user,
-                'status' => true,
-                'message' => 'User Created Successfully',
-                'token' => request()->createToken("API TOKEN")->plainTextToken
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], 500);
-        }
+        //     return response()->json([
+        //         'user' => $user,
+        //         'status' => true,
+        //         'message' => 'User Created Successfully',
+        //         'token' => request()->createToken("API TOKEN")->plainTextToken
+        //     ], 200);
+        // } catch (\Throwable $th) {
+        //     return response()->json([
+        //         'status' => false,
+        //         'message' => $th->getMessage()
+        //     ], 500);
+        // }
+        $user = User::create([
+            'fname' => request()->fname,
+            'lname' => request()->lname,
+            'email' => request()->email,
+            'contact' => request()->contact,
+            'current_residence' => request()->current_residence,
+            'profile' => request()->profile,
+            'gender' => request()->gender,
+            'chapter' => request()->chapter,
+            'grad_year' => request()->grad_year,
+            'password' => request()->password,
+            'role' => request()->role,
+            'inst' => request()->inst,
+        ]);
+
+        return response()->json([
+            'user' => $user,
+            'status' => true,
+            'message' => 'User Created Successfully',
+            'token' => request()->createToken("API TOKEN")->plainTextToken
+        ], 200);
     }
 
     public function show($id)
