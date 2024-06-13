560b123bfbcd8f3af45473bdf28bee2a50e4b017
abimon <eddyabby59@gmail.com>
2024-06-13 14:54:38 +0300

    delete student

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -21,22 +21,22 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        Log::channel("test")->info(json_encode(request('fname')));
-        User::create([
-            'fname'=>request('fname'),
-            'mname'=>request('mname'),
-            'lname'=>request('lname'),
-            'email'=>request('email'),
-            'contact'=>request('contact'),
-            'current_residence'=>request('current_residence'),
-            'profile'=>request('profile'),
-            'gender'=>request('gender'),
-            'chapter'=>request('chapter'),
-            'grad_year'=>request('grad_year'),
-            'password'=>request('password'),
-            'role'=>request('role'),
-            'inst'=>request('inst')
-        ]);
+        Log::channel("test")->info(request('email') .''. request('password'));
+        // User::create([
+        //     'fname'=>request('fname'),
+        //     'mname'=>request('mname'),
+        //     'lname'=>request('lname'),
+        //     'email'=>request('email'),
+        //     'contact'=>request('contact'),
+        //     'current_residence'=>request('current_residence'),
+        //     'profile'=>request('profile'),
+        //     'gender'=>request('gender'),
+        //     'chapter'=>request('chapter'),
+        //     'grad_year'=>request('grad_year'),
+        //     'password'=>request('password'),
+        //     'role'=>request('role'),
+        //     'inst'=>request('inst')
+        // ]);
         return response()->json(['message'=> 'Success'],200);
     }
 
