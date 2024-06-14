7393fe280bb4eb5029f7a9426df8cd4e0aee7c30
abimon <eddyabby59@gmail.com>
2024-06-14 13:48:22 +0300

    accounts

--- resources/views/dashboard/leaders/index.blade.php
+++ resources/views/dashboard/leaders/index.blade.php
@@ -6,13 +6,13 @@
     <div class="row">
         @foreach ($leaders as $leader)
         <div class="col-lg-4 col-md-6 p-2">
-            <div class="card h-100">
+            <div class="card">
                 <div class="card-body " >
                     <h5 class="card-title text-uppercase fw-bolder">{{$leader->user->fname}} {{$leader->user->lname}}</h5>
                     <h6 class="card-subtitle mb-2 text-body-secondary text-uppercase">
                     {{$leader->role->title}}
                     </h6>
-                    <div class="card-text">
+                    <div class="card-text h-100">
                         <?php echo html_entity_decode(mb_substr($leader->role->description,0,300)).'...'; ?>
                     </div>
                     <div class="d-flex justify-content-between">
