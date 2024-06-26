3e98b9b155becc01e0ed0f145728a29b8ac8157a
abimon <eddyabby59@gmail.com>
2024-06-27 00:40:24 +0300

    full view

--- resources/views/dashboard/events/index.blade.php
+++ resources/views/dashboard/events/index.blade.php
@@ -74,7 +74,7 @@
                         @if ((Auth()->user()->role == 'Activity Coordinator')||(Auth()->user()->role == 'Website'))
                         <div class="row">
                             <div class="col-4"><i class="bi bi-vector-pen" data-bs-toggle="modal" data-bs-target="#Edit{{$event->id}}"></i></div>
-                            <div class="col-4"><i class="bi bi-trash"></i></div>
+                            <div class="col-4"><i class="bi bi-trash text-danger" data-bs-toggle="modal" data-bs-target="#Delete{{$event->id}}"></i></div>
                         </div>
                         @endif
                     </div>
@@ -141,6 +141,27 @@
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                         <button type="submit" class="btn btn-primary">Update changes</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal fade" id="Delete{{$event->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Event</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <form action="{{route('event.destroy',$event->id)}}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('DELETE')
+                                    <div class="modal-body">
+                                        <p>Are you sure you would wish to delete this event.</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-delete">Delete</button>
                                     </div>
                                 </form>
                             </div>
