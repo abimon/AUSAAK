1ec79d3c2cd3dfb348cde1a45e3cd71a13c9ef82
abimon <eddyabby59@gmail.com>
2024-06-27 00:16:54 +0300

    edit

--- resources/views/dashboard/events/index.blade.php
+++ resources/views/dashboard/events/index.blade.php
@@ -69,21 +69,78 @@
         <div class="col-lg-4 col-md-6 p-2">
             <div class="card">
                 <div class="card-body ">
-                    <h5 class="card-title text-uppercase fw-bolder">{{$event->event_title}}</h5>
+
 
                     <div class="d-flex justify-content-between">
-                        <h6 class="card-subtitle mb-2 text-body-secondary text-uppercase">
-                            {{$event->event_date}}
-                        </h6>
-                        <div class="d-flex justify-content-between">
-                            <div class="col-4"><i class="bi bi-vector-pen"></i></div>
-                            <div class="col-4"><i class="bi bi-eye"></i></div>
-                        </div>
+                        <h5 class="card-title text-uppercase fw-bolder">{{$event->event_title}}</h5>
+                        <div class="col-4"><i class="bi bi-vector-pen" data-bs-toggle="modal" data-bs-target="#Edit{{$event->id}}"></i></div>
+                        <div class="col-4"><i class="bi bi-eye"></i></div>
                     </div>
+                    <h6 class="card-subtitle mb-2 text-body-secondary text-uppercase">
+                        {{$event->event_date}}
+                    </h6>
                     <div class="card-text">
                         <?php echo html_entity_decode(mb_substr($event->event_desc, 0, 300)); ?>...
                     </div>
-
+                    <div class="modal fade" id="Edit{{$event->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Event</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <form action="{{route('event.update',$event->id)}}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="modal-body">
+                                        <div class="form-floating mb-2">
+                                            <input type="text" name="event_title" value="{{$event->event_title}}" class="form-control">
+                                            <label for="">Title</label>
+                                        </div>
+                                        <div class="form-floating mb-2">
+                                            <input type="date" name="event_date" value="{{$event->event_date}}" class="form-control">
+                                            <label for="">Date</label>
+                                        </div>
+                                        <div class="form-floating mb-2">
+                                            <input type="time" name="from" value="{{$event->from}}" class="form-control">
+                                            <label for="">Time</label>
+                                        </div>
+                                        <div class="form-floating mb-2">
+                                            <input type="text" name="{{$event->event_desc}}" value="event_desc" class="form-control">
+                                            <label for="">Description</label>
+                                        </div>
+                                        <div class="">
+                                            <label for="">Cover Image</label>
+                                            <div class="m-3 card p-3 border-dark bg-transparent" style="border-style:dashed;">
+                                                <img id="out" src="" style="width: 100%; object-fit:contain;" />
+                                                <input type="file" accept="image/jpeg, image/png, image/webp" name="cover" id="cover" style="display: none;" class="form-control" onchange="loadCoverFile(event)">
+                                                <div class="pt-2" id="desc">
+                                                    <div class="text-center" style="font-size: xxx-large; font-weight:bolder;">
+                                                        <i class="bi bi-upload"></i>
+                                                    </div>
+                                                    <div class="text-center">
+                                                        <label for="cover" class="btn btn-success text-white" title="Upload new profile image">Browse</label>
+                                                    </div>
+                                                    <div class="text-center prim">*File supported .png .jpg .webp</div>
+                                                </div>
+                                                <script>
+                                                    var loadCoverFile = function(event) {
+                                                        var image = document.getElementById('out');
+                                                        image.src = URL.createObjectURL(event.target.files[0]);
+                                                        document.getElementById('cover').value == image.src;
+                                                    };
+                                                </script>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
